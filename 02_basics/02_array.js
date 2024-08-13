33592bab83f6a3f3ded93547f37eed5511ac6d90
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2024-08-13 05:33:21 +0000

    array 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,34 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)  // concat rerurn new array
+// console.log(allHeros);
+
+// +++ other way->> sprid oprater +++
+
+const all_new_heros = [...marvel_heros, ...dc_heros] 
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Rustam"))
+console.log(Array.from("Rustam")) // Convert new array
+console.log(Array.from({name: "hitesh"})) // interesting
+//empty arrry.-> confuse key or valu se array banye
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+// of return set of new elements, convertig
