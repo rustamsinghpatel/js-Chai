92074a7bf948b5d35993a2ac82133a11caf231ad
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2024-08-20 04:43:39 +0000

    forEach loop

---
+++ 05_iterationsLoop/five.js
@@ -0,0 +1,41 @@
+// forEach --> some loop automatic inject for array
+// callback --> no name ,
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+// arro fun-->>
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
