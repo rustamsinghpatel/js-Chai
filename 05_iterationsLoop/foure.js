3d5d8cb472493f3cb60749874bdcbb3b3c09703c
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2024-08-20 04:29:55 +0000

    for in

---
+++ 05_iterationsLoop/foure.js
@@ -0,0 +1,27 @@
+// for in not only object
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map() // not itratbal
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
